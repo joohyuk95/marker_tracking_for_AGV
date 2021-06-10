8ab79536cb33e20002779d4fa6d90f6a83927e39
flpdsc <92yjkim@gmail.com>
2021-06-10 16:45:17 +0900

    python code changed. python manu_bg.py [ tb2 | tb3 ]

--- manu_bg.py
+++ manu_bg.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 from serial import Serial
+
+if len(sys.argv) != 2:
+        print("Usage: python manu_bg.py [tb2|tb3]")
+        sys.exit()
 
 ser = Serial('/dev/ttyMANU', 115200)
 
 str0 = '0'
 str1 = '1'
 
+tb_id = sys.argv[1]
+
 while True:
-        tb_id = rospy.get_param('/tb_id')
         pa = rospy.get_param('/'+tb_id+'/mission')
+
         if pa == True:
                 ser.write(str0.encode())
                 rospy.sleep(2)
