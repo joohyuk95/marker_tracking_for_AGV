e94f7f00d67439b19bfe1e6360ce3483a088e03c
flpdsc <92yjkim@gmail.com>
2021-06-10 17:04:35 +0900

    python code changed. python manu_bg.py [tb2|tb3] 로 실행 /tb_id/mission_start, /tb_id/mission_finished와 함께 /tb_id/mission 파라미터를 이용하여 물건을 잡았다가 놓도록 함

--- manu_bg.py
+++ manu_bg.py
@@ -5,8 +5,8 @@ import rospy
 from serial import Serial
 
 if len(sys.argv) != 2:
-        print("Usage: python manu_bg.py [tb2|tb3]")
-        sys.exit()
+    print("Usage: python manu_bg.py [tb2|tb3]")
+    sys.exit()
 
 ser = Serial('/dev/ttyMANU', 115200)
 
@@ -16,10 +16,15 @@ str1 = '1'
 tb_id = sys.argv[1]
 
 while True:
-        pa = rospy.get_param('/'+tb_id+'/mission')
+    if rospy.get_param('/'+tb_id+'/mission_start') == True and rospy.get_param('/'+tb_id+'/mission') == True:
+        ser.write(str0.encode())
+        rospy.sleep(2)
+        ser.write(str1.encode())
+        break
 
-        if pa == True:
-                ser.write(str0.encode())
-                rospy.sleep(2)
-                ser.write(str1.encode())
-                break
+while True:
+    if rospy.get_param('/'+tb_id+'/mission_finished') == True and rospy.get_param('/'+tb_id+'/mission') == False:
+        ser.write(str1.encode())
+        rospy.sleep(2)
+        ser.write(str0.encode())
+        break
